2e27ba6e7d8a0f0c2f7a2b7a7740ec2fb40df7b1
Jeff Klenzing <jklenzing@gmail.com>
2020-06-08 11:25:30 -0400

    TST: update test with interp_model_onto_inst

--- pysatModels/tests/test_utils_extract.py
+++ pysatModels/tests/test_utils_extract.py
@@ -33,7 +33,7 @@ class TestUtilsExtractObsViewModel:
     def test_str_coords(self):
         """ Test string coordinate input"""
         self.args[2] = self.args[2][0]
-        extract.instrument_view_through_model(*self.args)
+        extract.interp_model_onto_inst(*self.args)
 
         for label in self.args[3]:
             assert "model_{:s}".format(label) in self.args[1].data.columns
@@ -41,7 +41,7 @@ class TestUtilsExtractObsViewModel:
     def test_str_obs(self):
         """ Test string model observation input"""
         self.args[3] = self.args[3][0]
-        extract.instrument_view_through_model(*self.args)
+        extract.interp_model_onto_inst(*self.args)
 
         assert "model_{:s}".format(self.args[3]) in self.args[1].data.columns
 
